3c5f60f6e2041874eab66e4716a18a1f1503fcb7
martin-b-07e5 <martin.bergagno@gmail.com>
2023-08-05 21:32:00 -0300

    requirements file an minor change

--- TP_final_L2/api_key_utils.py
+++ TP_final_L2/api_key_utils.py
@@ -1,11 +1,12 @@
 # api_key_utils.py
 
 import configparser
-
+import os
 
 def api_key_utils():
     config = configparser.ConfigParser()    # Creates a ConfigParser object
-    config.read('config.ini')    # Read the configuration file
+    # config.read('config.ini')    # Read the configuration file
+    config.read(os.path.join(os.path.dirname(__file__), 'config.ini'))
     api_key = config.get('WeatherAPI', 'api_key')
 
     return api_key
